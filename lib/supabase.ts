c734001b8e945431d3354ec1b7880fb7d53c9740
Lucho <lucianoagostini12@gmail.com>
2026-01-13 14:14:37 -0300

    deploy

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,17 +1,13 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr"
+import type { Database } from "@/types/supabase"
 
-// Variable "memoria" para guardar la conexión y no perderla
-let client: ReturnType<typeof createBrowserClient> | undefined
+let client: ReturnType<typeof createBrowserClient<Database>> | undefined
 
 export function createClient() {
-  // 1. Si ya existe una conexión viva, la devolvemos (REUTILIZACIÓN)
   if (client) return client
-
-  // 2. Si no existe, creamos una nueva y la guardamos
-  client = createBrowserClient(
+  client = createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
-  
   return client
 }

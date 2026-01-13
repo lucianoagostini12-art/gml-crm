38d7362853ffc3f884f20437257fd1ef798d41c3
Lucho <lucianoagostini12@gmail.com>
2026-01-13 14:17:24 -0300

    deploy

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,9 @@
-import { createBrowserClient } from "@supabase/ssr"
-import type { Database } from "@/types/supabase"
+import { createBrowserClient } from '@supabase/ssr'
 
-let client: ReturnType<typeof createBrowserClient<Database>> | undefined
-
+// FIJATE QUE TENGA LA PALABRA "export" AL PRINCIPIO
 export function createClient() {
-  if (client) return client
-  client = createBrowserClient<Database>(
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
-  return client
 }
